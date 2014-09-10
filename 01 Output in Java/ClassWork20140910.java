5d773e26034d42c21779b2b183b3a41aad2586de
ma <ma@fosslabs.ru>
2014-09-10 21:55:55 +0400

    dz 003-008, output examples in java

---
+++ 01 Output in Java/ClassWork20140910.java
@@ -0,0 +1,21 @@
+/**
+ * Created by ma on 10.09.14.
+ */
+public class ClassWork20140910 {
+
+    public static void main(String[] args) {
+
+        System.out.println("123"); //123
+        int x = 2;
+        System.out.println(x + 4); //6
+        System.out.println("x" + 4); //x4
+        int y = 4;
+        System.out.println(x + "" + y); //24
+        System.out.println(x + y + ""); //6
+        char c = 'A';
+        System.out.println(c + 1); //66
+        System.out.println((char)(c + 1));
+        System.out.println(c + 'A'); //130
+    }
+
+}
